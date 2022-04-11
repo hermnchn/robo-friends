6e9c97d711ed7a4b8c57f114d49c614b3c0cbd3f
Herman <hermnchn@gmail.com>
2022-04-10 23:38:26 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
